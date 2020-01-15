84f6c32e591ff87de8f8e6b16a821f918d043ad0
zhaohengping <zhaohengping@gongchang.com>
2020-01-15 16:50:37 +0800

    dns

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
+import os
 from setuptools import setup, find_packages
-
-try:
-    from wheel.bdist_wheel import bdist_wheel
-    HAS_WHEEL = True
-except ImportError:
-    HAS_WHEEL = False
 
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
+
+
 setup(
     name="tinydns",
     packages=find_packages(),
-    version = '0.0.2',
+    version = '0.0.1',
     description='this project is a tinydns automaton implementation by python',
     long_description=long_description,
     long_description_content_type="text/markdown",
