b362976e52cc59a6d5384e39fc48da4e308ad3a9
zhaohengping <zhaohengping@gongchang.com>
2020-01-17 10:37:44 +0800

    dns

--- setup.py
+++ setup.py
@@ -1,28 +1,8 @@
 import os
 import sys
 from setuptools import setup, find_packages
-from setuptools.command.install import install
-from setuptools.command.install_lib import install_lib
-from setuptools.command.build_ext import build_ext
-
-
 with open("README.md", "r") as f:
     long_description = f.read()
-
-def patch_bin_path(cmd, conf):
-
-    bin_name = conf.get('bin_name')
-
-    if not os.path.isabs(bin_name):
-        print('Patching "bin_name" to properly install_scripts dir')
-        try:
-            if not os.path.exists(cmd.install_scripts):
-                os.makedirs(cmd.install_scripts)
-            conf.set('bin_name',
-                     os.path.join(cmd.install_scripts, conf.get('bin_name')))
-        except Exception:
-            conf.set('bin_name', sys.prefix + '/bin/' + bin_name)
-
 
 setup(
     name="tinydns",
@@ -39,7 +19,7 @@ setup(
     install_requires = ["gevent","dnslib","redis"],
     entry_points = {
                   'console_scripts': [
-                      'tinydns = tinydns.__init__:tinydns',
+                      'tinydns = tinydns.__init__:main',
                   ],
               },
     package_data={
--- tinydns/__init__.py
+++ tinydns/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import sys
+import argparse
+import os
 from gevent import socket
 import gevent
 from gevent import monkey
@@ -10,7 +13,6 @@ cf = ConfigParser.ConfigParser()
 # 读取配置文件
 # /etc/tinydns.conf
 cf.read("../tinydns.conf")
-
 A_RECORD_PREFIX = cf.get('dns','A_RECORD_PREFIX')
 TXT_RECORD_PREFIX = cf.get('dns','TXT_RECORD_PREFIX')
 CNAME_RECORD_PREFIX = cf.get('dns','CNAME_RECORD_PREFIX')
@@ -19,7 +21,6 @@ SOCK_DGRAM = cf.get('gevent','AF_INET')
 s = socket.socket(int(AF_INET), int(SOCK_DGRAM))
 port = int(cf.get('dns','port'))
 s.bind(('', port))
-
 
 def dns_handler(s, peer, data,r):
     request = DNSRecord.parse(data)
@@ -36,7 +37,6 @@ def dns_handler(s, peer, data,r):
             print e
             print ('Host not found')
             IP = '0.0.0.0'
-    print ("Request (%s): %r (%s) - Response: %s" % (str(peer), qname.label,QTYPE[qtype], IP))
     reply = DNSRecord(DNSHeader(id=id, qr=1, aa=1, ra=1), q=request.q)
     if qtype == QTYPE.A:
         reply.add_answer(RR(qname, qtype, rdata=A(IP)))
@@ -49,7 +49,73 @@ def dns_handler(s, peer, data,r):
     s.sendto(reply.pack(), peer)
 
 
-def tinydns():
+try:
+    import resource
+except ImportError:
+    resource = None
+MAXFD = 1024
+if hasattr(os, "devnull"):
+    REDIRECT_TO = os.devnull  # PRAGMA: NOCOVER
+else:
+    REDIRECT_TO = "/dev/null"  # PRAGMA: NOCOVER
+
+def get_maxfd():
+    if not resource:
+        maxfd = MAXFD
+    else:
+        maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+        if maxfd == resource.RLIM_INFINITY:
+            maxfd = MAXFD
+    return maxfd
+
+try:
+    from os import closerange
+except ImportError:
+    def closerange(fd_low, fd_high):    # NOQA
+        # Iterate through and close all file descriptors.
+        for fd in range(fd_low, fd_high):
+            try:
+                os.close(fd)
+            except OSError:    # ERROR, fd wasn't open to begin with (ignored)
+                pass
+
+def daemonize():
+    """Standard daemonization of a process.
+    """
+    # guard to prevent daemonization with gevent loaded
+    for module in sys.modules.keys():
+        if module.startswith('gevent'):
+            raise ValueError('Cannot daemonize if gevent is loaded')
+
+    if hasattr(os, 'fork'):
+        child_pid = os.fork()
+    else:
+        raise ValueError("Daemonizing is not available on this platform.")
+
+    if child_pid != 0:
+        # we're in the parent
+        os._exit(0)
+
+    # child process
+    os.setsid()
+
+    subchild = os.fork()
+    if subchild:
+        os._exit(0)
+
+    # subchild
+    maxfd = get_maxfd()
+    closerange(0, maxfd)
+    os.open(REDIRECT_TO, os.O_RDWR)
+    os.dup2(0, 1)
+    os.dup2(0, 2)
+
+def main():
+    parser = argparse.ArgumentParser(description='Run some watchers.')
+    parser.add_argument('--c', dest='daemonize', action='store_true',
+                        help="Start tinydns in the background. Not supported "
+                             "on Windows")
+    args = parser.parse_args()
     host = cf.get('db','db_host')
     port = cf.get('db','db_port')
     db = cf.get('db','db')
@@ -60,4 +126,5 @@ def tinydns():
 
 
 if __name__ == '__main__':
-    tinydns()
+    main()
+
