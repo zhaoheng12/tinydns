7c3e4d75a9d40e9de280c04a106169820b375912
zhaohengping <zhaohengping@gongchang.com>
2020-01-17 14:13:33 +0800

    dns

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
     url='https://github.com/zhaoheng12/tinydns/',
     author='zhaohengping',
     author_email='zhaohengping@gongchang.com',
-    data_files=['tinydns.conf'],
-    package_dir={'':'etc'},
+    data_files=('etc', ['tinydns.conf']),
+    # package_dir={'tinydns.conf':'etc'},
     classifiers=[
           "Programming Language :: Python",
           "Programming Language :: Python :: 2.7.15",
