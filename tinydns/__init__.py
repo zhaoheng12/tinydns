d901f240375fce1cf41c5aae903ef4a9bf4c3c00
zhaohengping <zhaohengping@gongchang.com>
2020-01-17 13:36:19 +0800

    dns

--- tinydns/__init__.py
+++ tinydns/__init__.py
@@ -10,9 +10,13 @@ cf = ConfigParser.ConfigParser()
 # 读取配置文件
 # /etc/tinydns.conf
 cf.read("../tinydns.conf")
-AF_INET = cf.get('gevent_dns','AF_INET')
-SOCK_DGRAM = cf.get('gevent_dns','AF_INET')
-port = cf.get('gevent_dns','port')
+# AF_INET = cf.get('gevent_dns','AF_INET')
+# SOCK_DGRAM = cf.get('gevent_dns','AF_INET')
+# port = cf.get('gevent_dns','port')
+
+AF_INET = 2
+SOCK_DGRAM = 2
+port = 53
 s = socket.socket(int(AF_INET), int(SOCK_DGRAM))
 s.bind(('', int(port)))
 
@@ -27,6 +31,7 @@ def dns_handler(s, peer, data):
         print e
         print ('Host not found')
         IP = '0.0.0.0'
+    print ("Request (%s): %r (%s) - Response: %s" % (str(peer), qname.label,QTYPE[qtype], IP))
     reply = DNSRecord(DNSHeader(id=id, qr=1, aa=1, ra=1), q=request.q)
     if qtype == QTYPE.A:
         reply.add_answer(RR(qname, qtype, rdata=A(IP)))
@@ -41,7 +46,7 @@ def dns_handler(s, peer, data):
 
 def main():
     parser = argparse.ArgumentParser(description='Run some watchers.')
-    parser.add_argument('-c', type=str, action='store_true',default='../tinydns.conf',
+    parser.add_argument('-c', type=str,default='../tinydns.conf',
                         help="Run service command tinydns -c /etc/tinydns.conf")
     args = parser.parse_args()
     while True:
