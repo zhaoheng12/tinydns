20a3ececbac7c9015a1d77e548c117a3e5ff4bea
zhaohengping <zhaohengping@gongchang.com>
2020-01-18 16:20:09 +0800

    dns

--- examples/test_cli.py
+++ examples/test_cli.py
@@ -1,27 +1,7 @@
 # -*- coding: utf-8 -*-
-# import os
-# aa = os.system("dig @127.0.0.1 zhplz.com")
-# print (aa)
-import sys, getopt
+import os
+aa = os.system("dig @127.0.0.1 zhplz.com")
+print (aa)
 
-def main(argv):
-
-   print '=============='
-   inputfile = '666'
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print 'test.py -i <inputfile> -o <outputfile>'
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt in ("-i", "--ifile"):
-         inputfile = arg
-
-   print '输入的文件为：',
-
-
-if __name__ == "__main__":
-   main(sys.argv[1:])
 
 
